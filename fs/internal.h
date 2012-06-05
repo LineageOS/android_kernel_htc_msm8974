006e1b2e884d48b4544604286780008ecfe845d9
Miklos Szeredi <mszeredi@suse.cz>
2012-06-05 15:10:27 +0200

    vfs: remove open intents from nameidata
    All users of open intents have been converted to use ->atomic_{open,create}.
    
    This patch gets rid of nd->intent.open and related infrastructure.
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/internal.h
+++ fs/internal.h
@@ -82,13 +82,10 @@ extern int umount2(char *name, int flags);
 /*
  * open.c
  */
-struct nameidata;
-extern struct file *nameidata_to_filp(struct nameidata *);
-extern void release_open_intent(struct nameidata *);
 struct opendata {
 	struct dentry *dentry;
 	struct vfsmount *mnt;
-	struct file **filp;
+	struct file *filp;
 };
 struct open_flags {
 	int open_flag;
