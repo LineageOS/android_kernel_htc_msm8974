3a22026bc7901e0435c54a13be08fdf2a3deedd6
David Rientjes <rientjes@google.com>
2012-07-31 16:43:48 -0700

    mm, memcg: move all oom handling to memcontrol.c
    By globally defining check_panic_on_oom(), the memcg oom handler can be
    moved entirely to mm/memcontrol.c.  This removes the ugly #ifdef in the
    oom killer and cleans up the code.
    
    Signed-off-by: David Rientjes <rientjes@google.com>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Change-Id: Ibe765fddf4728b7392218518eb50380c571b4274

--- include/linux/oom.h
+++ include/linux/oom.h
@@ -64,6 +64,9 @@ extern void note_oom_kill(void);
 extern int try_set_zonelist_oom(struct zonelist *zonelist, gfp_t gfp_flags);
 extern void clear_zonelist_oom(struct zonelist *zonelist, gfp_t gfp_flags);
 
+extern void check_panic_on_oom(enum oom_constraint constraint, gfp_t gfp_mask,
+			       int order, const nodemask_t *nodemask);
+
 extern enum oom_scan_t oom_scan_process_thread(struct task_struct *task,
 		unsigned long totalpages, const nodemask_t *nodemask,
 		bool force_kill);
