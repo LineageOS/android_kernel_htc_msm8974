755d8fb678dbd52d25d90d6c29e4a06ca5171ae5
David Rientjes <rientjes@google.com>
2012-11-12 17:53:04 -0800

    mm, oom: reintroduce /proc/pid/oom_adj
    This is mostly a revert of 01dc52ebdf47 ("oom: remove deprecated oom_adj")
    from Davidlohr Bueso.
    
    It reintroduces /proc/pid/oom_adj for backwards compatibility with earlier
    kernels.  It simply scales the value linearly when /proc/pid/oom_score_adj
    is written.
    
    The major difference is that its scheduled removal is no longer included
    in Documentation/feature-removal-schedule.txt.  We do warn users with a
    single printk, though, to suggest the more powerful and supported
    /proc/pid/oom_score_adj interface.
    
    Reported-by: Artem S. Tashkinov <t.artem@lycos.com>
    Signed-off-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Change-Id: I0f22c89bc3cb5d86505710713d8ae34b0c063ec7

--- include/linux/oom.h
+++ include/linux/oom.h
@@ -8,6 +8,15 @@
 #define OOM_SCORE_ADJ_MIN	(-1000)
 #define OOM_SCORE_ADJ_MAX	1000
 
+/*
+ * /proc/<pid>/oom_adj set to -17 protects from the oom killer for legacy
+ * purposes.
+ */
+#define OOM_DISABLE (-17)
+/* inclusive */
+#define OOM_ADJUST_MIN (-16)
+#define OOM_ADJUST_MAX 15
+
 #ifdef __KERNEL__
 
 #include <linux/sched.h>
