566a2a9fef3386fcf28db557db4475ce061b753f
flar2 <asegaert@gmail.com>
2014-05-23 00:22:05 -0400

    kexec-hardboot: needed for MultiROM host compatibility
    based on @Tasssadar
    https://github.com/Tasssadar/android_kernel_google_msm/
    commit/005cf387c1404eac862cc35153d7641d18faef4c
    
    Signed-off-by: flar2 <asegaert@gmail.com>
    
    Conflicts:
    	arch/arm/include/asm/kexec.h
    	arch/arm/kernel/machine_kexec.c
    	arch/arm/mach-msm/board-htc-8974.c
    	include/linux/kexec.h
    
    Change-Id: Ibd397c898fd037d834c30df4f3334bacbe5bc58e

--- arch/arm/include/asm/kexec.h
+++ arch/arm/include/asm/kexec.h
@@ -14,6 +14,10 @@
 #define KEXEC_ARM_ATAGS_OFFSET  0x1000
 #define KEXEC_ARM_ZIMAGE_OFFSET 0x8000
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+  #define KEXEC_HB_PAGE_MAGIC 0x4a5db007
+#endif
+
 #ifndef __ASSEMBLY__
 
 static inline void crash_setup_regs(struct pt_regs *newregs,
@@ -41,7 +45,11 @@ static inline void crash_setup_regs(struct pt_regs *newregs,
 
 extern void (*kexec_reinit)(void);
 
-#endif 
+#ifdef CONFIG_KEXEC_HARDBOOT
+extern void (*kexec_hardboot_hook)(void);
+#endif
+
+#endif
 
 #endif 
 
