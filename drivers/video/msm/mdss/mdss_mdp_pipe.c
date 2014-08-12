3a57da30e1d7b826e671803f2500f0a5f00edf5a
Justin Philip <jphili@codeaurora.org>
2014-08-12 13:46:54 +0530

    mdss: Add MDP_SMP_FORCE_ALLOC mdp flag
    MDP_SMP_FORCE_ALLOC flag is used to allow SMP
    allocations even when there is mismatch between
    allocated and requested SMPs for a pipe.
    User space can make use of this flag in extreme scenarios
    where SMP allocations need to pass like GPU composition
    and playback of protected  or secure videos.
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_mdp_pipe.c
    
    Change-Id: I369b4361e7e2bbfc8150add467678c4ef8d5cfb6
    Signed-off-by: Justin Philip <jphili@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_pipe.c
+++ drivers/video/msm/mdss/mdss_mdp_pipe.c
@@ -221,6 +221,8 @@ int mdss_mdp_smp_reserve(struct mdss_mdp_pipe *pipe)
 
 	nlines = pipe->bwc_mode ? 1 : 2;
 
+	force_alloc = pipe->flags & MDP_SMP_FORCE_ALLOC;
+
 	mutex_lock(&mdss_mdp_smp_lock);
 	for (i = (MAX_PLANES - 1); i >= ps.num_planes; i--) {
 		if (bitmap_weight(pipe->smp_map[i].allocated, SMP_MB_CNT)) {
