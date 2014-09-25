3e33ada51da552a38390435005fcc889e19193e1
Zefan Li <lizefan@huawei.com>
2014-09-25 09:40:17 +0800

    sched: fix confusing PFA_NO_NEW_PRIVS constant
    Commit 1d4457f99928 ("sched: move no_new_privs into new atomic flags")
    defined PFA_NO_NEW_PRIVS as hexadecimal value, but it is confusing
    because it is used as bit number. Redefine it as decimal bit number.
    
    Note this changes the bit position of PFA_NOW_NEW_PRIVS from 1 to 0.
    
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Miao Xie <miaox@cn.fujitsu.com>
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Acked-by: Kees Cook <keescook@chromium.org>
    [ lizf: slightly modified subject and changelog ]
    Signed-off-by: Zefan Li <lizefan@huawei.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    
    Change-Id: I7ddd900ffe1c467bb0141319268c0913d4684c9e

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1885,9 +1885,9 @@ extern int task_free_unregister(struct notifier_block *n);
 #define used_math() tsk_used_math(current)
 
 /* Per-process atomic flags. */
+#define PFA_NO_NEW_PRIVS 0	/* May not gain new privileges. */
 #define PFA_SPREAD_PAGE  1      /* Spread page cache over cpuset */
 #define PFA_SPREAD_SLAB  2      /* Spread some slab caches over cpuset */
-#define PFA_NO_NEW_PRIVS 0x00000001	/* May not gain new privileges. */
 
 #define TASK_PFA_TEST(name, func)					\
 	static inline bool task_##func(struct task_struct *p)		\
