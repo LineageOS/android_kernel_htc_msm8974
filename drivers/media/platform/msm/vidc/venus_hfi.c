5d52072555677e46fb6adaa7b3afb7b856a48904
Abdulla Anam <abdullahanam@codeaurora.org>
2016-06-03 17:39:42 +0530

    msm: vidc: use %pK instead of %p which respects kptr_restrict sysctl
    Hide kernel pointers from unprivileged ussers by using %pK format-
    specifier instead of %p. This respects the kptr_restrict sysctl
    setting which is by default on. So by default %pK will print zeroes
    as address. echo 1 to kptr_restrict to print proper kernel addresses.
    
    CRs-Fixed: 987018
    Change-Id: I4772257a557c6730ecc0624cbc8e5614e893e9fd
    Signed-off-by: Abdulla Anam <abdullahanam@codeaurora.org>
    Signed-off-by: Bikshapathi Kothapeta <bkotha@codeaurora.org>

--- drivers/media/platform/msm/vidc/venus_hfi.c
+++ drivers/media/platform/msm/vidc/venus_hfi.c
@@ -657,7 +657,7 @@ static int venus_hfi_unvote_bus(void *dev,
 	struct venus_hfi_device *device = dev;
 
 	if (!device) {
-		dprintk(VIDC_ERR, "%s invalid device handle %p",
+		dprintk(VIDC_ERR, "%s invalid device handle %pK",
 			__func__, device);
 		return -EINVAL;
 	}
@@ -752,7 +752,7 @@ static int venus_hfi_scale_bus(void *dev, int load,
 	int bus_vector = 0;
 
 	if (!device) {
-		dprintk(VIDC_ERR, "%s invalid device handle %p",
+		dprintk(VIDC_ERR, "%s invalid device handle %pK",
 			__func__, device);
 		return -EINVAL;
 	}
@@ -1062,7 +1062,7 @@ static int __unset_free_ocmem(struct venus_hfi_device *device)
 {
 	int rc = 0;
 	if (!device || !device->res) {
-		dprintk(VIDC_ERR, "%s Invalid param, device: 0x%p\n",
+		dprintk(VIDC_ERR, "%s Invalid param, device: 0x%pK\n",
 				__func__, device);
 		return -EINVAL;
 	}
@@ -2453,7 +2453,7 @@ static int venus_hfi_session_clean(void *session)
 		return -EINVAL;
 	}
 	sess_close = session;
-	dprintk(VIDC_DBG, "deleted the session: 0x%p",
+	dprintk(VIDC_DBG, "deleted the session: 0x%pK",
 			sess_close);
 	mutex_lock(&((struct venus_hfi_device *)
 			sess_close->device)->session_lock);
@@ -3305,7 +3305,7 @@ static int venus_hfi_register_iommu_domains(struct venus_hfi_device *device,
 		domain = iommu_group_get_iommudata(iommu_map->group);
 		if (!domain) {
 			dprintk(VIDC_ERR,
-				"Failed to get domain data for group %p",
+				"Failed to get domain data for group %pK",
 				iommu_map->group);
 			rc = -EINVAL;
 			goto fail_group;
@@ -3313,7 +3313,7 @@ static int venus_hfi_register_iommu_domains(struct venus_hfi_device *device,
 		iommu_map->domain = msm_find_domain_no(domain);
 		if (iommu_map->domain < 0) {
 			dprintk(VIDC_ERR,
-				"Failed to get domain index for domain %p",
+				"Failed to get domain index for domain %pK",
 				domain);
 			rc = -EINVAL;
 			goto fail_group;
