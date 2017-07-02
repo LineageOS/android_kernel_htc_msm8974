0511c6be5cc64bfb935555e05d83759a3d75161b
Bruno Martins <bgcngm@gmail.com>
2017-07-02 22:27:30 +0100

    [WIP] Import HTC One M8 Google Play Edition kernel source
     * Based on HTC package 5.07.1700.6 (Android M)
     * Imported over caf/LA.BF.1.1.3-00810-8x74.0
    
     * Additional notes:
        - Integrated HTC sound changes into sound/soc/msm/msm8974.c
        - Moved drivers/i2c/chips into more appropriate directories
        - Removed useless debugging and excessive logging
        - Removed unused drivers
    
    Change-Id: I49a0afbaf60c30624e26750931b0cd109f761ac6

--- include/sound/q6adm-v2.h
+++ include/sound/q6adm-v2.h
@@ -70,4 +70,7 @@ void adm_set_multi_ch_map(char *channel_map);
 
 void adm_get_multi_ch_map(char *channel_map);
 
+int q6adm_enable_effect(int port_id, uint32_t module_id, uint32_t param_id,
+		uint32_t payload_size, void *payload);
+
 #endif /* __Q6_ADM_V2_H__ */
