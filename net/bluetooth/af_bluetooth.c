9e0b864184cf54ed249dd68f85618ea3cfafd605
Bruno Martins <bgcngm@gmail.com>
2017-08-14 14:45:46 +0100

    net: bluetooth: Reset to CAF
     * Something is messing pretty bad with the previously existing
       codeaurora code base, causing BT disconnetions
    
    Change-Id: I4395105a57fd5b428699cbfd13ff5c2c4604ae54

--- net/bluetooth/af_bluetooth.c
+++ net/bluetooth/af_bluetooth.c
@@ -292,6 +292,8 @@ int bt_sock_recvmsg(struct kiocb *iocb, struct socket *sock,
 		return err;
 	}
 
+	msg->msg_namelen = 0;
+
 	copied = skb->len;
 	if (len < copied) {
 		msg->msg_flags |= MSG_TRUNC;
@@ -348,6 +350,8 @@ int bt_sock_stream_recvmsg(struct kiocb *iocb, struct socket *sock,
 
 	if (flags & MSG_OOB)
 		return -EOPNOTSUPP;
+
+	msg->msg_namelen = 0;
 
 	BT_DBG("sk %p size %zu", sk, size);
 
