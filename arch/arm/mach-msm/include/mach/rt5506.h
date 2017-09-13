f48f6e7dc9d37667e06a277b28fb36b1e820d50c
Bruno Martins <bgcngm@gmail.com>
2017-09-13 23:30:35 +0100

    ASoC: rt5506: Refactor headphone amp driver
     * Adopt the new way of handling IOCTL calls as seen in the
       newest rt5506 drivers released by HTC for A9 (hiae).
    
     * This is just an easy and quick way to fix the problem discovered
       recently of improper userspace interaction.
    
    Change-Id: I7f5b2b7169f179201b8bc2b8e0a0479e8c1f08e4

--- arch/arm/mach-msm/include/mach/rt5506.h
+++ arch/arm/mach-msm/include/mach/rt5506.h
@@ -13,72 +13,49 @@
 #define RT5506_I2C_NAME "rt5506"
 #define MAX_REG_DATA 15
 
-struct rt5506_platform_data {
-	uint32_t gpio_rt5506_enable;
-	const char *power_supply;
-	struct rpm_regulator *power_reg;
-};
-
-struct rt5506_reg_data {
-	unsigned char addr;
-	unsigned char val;
-};
-
-struct rt5506_config {
-	unsigned int reg_len;
-	struct rt5506_reg_data reg[MAX_REG_DATA];
-};
-
-struct rt5506_comm_data {
-	unsigned int out_mode;
-	struct rt5506_config config;
-};
-
-struct rt5506_config_data {
-	unsigned int mode_num;
-	struct rt5506_comm_data *cmd_data;  /* [mode][mode_kind][reserve][cmds..] */
-};
+#define QUERY_IMMED		msecs_to_jiffies(0)
+#define QUERY_LATTER		msecs_to_jiffies(200)
+#define AMP_SENSE_READY		0x80
 
 enum {
-	AMP_INIT = 0,
-	AMP_MUTE,
-	AMP_MAX_FUNC
+	RT55XX_INIT = 0,
+	RT55XX_MUTE,
+	RT55XX_MAX_FUNC
 };
 
-enum PLAYBACK_MODE {
-	PLAYBACK_MODE_OFF = AMP_MAX_FUNC,
-	PLAYBACK_MODE_PLAYBACK,
-	PLAYBACK_MODE_PLAYBACK8OH,
-	PLAYBACK_MODE_PLAYBACK16OH,
-	PLAYBACK_MODE_PLAYBACK32OH,
-	PLAYBACK_MODE_PLAYBACK64OH,
-	PLAYBACK_MODE_PLAYBACK128OH,
-	PLAYBACK_MODE_PLAYBACK256OH,
-	PLAYBACK_MODE_PLAYBACK500OH,
-	PLAYBACK_MODE_PLAYBACK1KOH,
-	PLAYBACK_MODE_VOICE,
-	PLAYBACK_MODE_TTY,
-	PLAYBACK_MODE_FM,
-	PLAYBACK_MODE_RING,
-	PLAYBACK_MODE_MFG,
-	PLAYBACK_MODE_BEATS_8_64,
-	PLAYBACK_MODE_BEATS_128_500,
-	PLAYBACK_MODE_MONO,
-	PLAYBACK_MODE_MONO_BEATS,
-	PLAYBACK_MAX_MODE
+enum RT55XX_MODE {
+	RT55XX_MODE_OFF = RT55XX_MAX_FUNC,
+	RT55XX_MODE_PLAYBACK,
+	RT55XX_MODE_PLAYBACK8OH,
+	RT55XX_MODE_PLAYBACK16OH,
+	RT55XX_MODE_PLAYBACK32OH,
+	RT55XX_MODE_PLAYBACK64OH,
+	RT55XX_MODE_PLAYBACK128OH,
+	RT55XX_MODE_PLAYBACK256OH,
+	RT55XX_MODE_PLAYBACK500OH,
+	RT55XX_MODE_PLAYBACK1KOH,
+	RT55XX_MODE_VOICE,
+	RT55XX_MODE_TTY,
+	RT55XX_MODE_FM,
+	RT55XX_MODE_RING,
+	RT55XX_MODE_MFG,
+	RT55XX_MODE_BEATS_8_64,
+	RT55XX_MODE_BEATS_128_500,
+	RT55XX_MODE_MONO,
+	RT55XX_MODE_MONO_BEATS,
+	RT55XX_MAX_MODE
 };
 
 enum HEADSET_QUERY_STATUS {
-	QUERY_OFF = 0,
-	QUERY_HEADSET,
-	QUERY_FINISH,
+	RT55XX_QUERY_OFF = 0,
+	RT55XX_QUERY_HEADSET,
+	RT55XX_QUERY_FINISH,
 };
 
-
-enum AMP_STATUS {
-	STATUS_OFF = 0,
-	STATUS_PLAYBACK,
-	STATUS_SUSPEND,
+enum RT55XX_STATUS {
+	RT55XX_OFF = 0,
+	RT55XX_PLAYBACK,
+	RT55XX_SUSPEND,
 
 };
 
@@ -96,27 +73,49 @@ enum HEADSET_OM {
 };
 
 enum AMP_GPIO_STATUS {
-	 AMP_GPIO_OFF = 0,
-	 AMP_GPIO_ON,
-	 AMP_GPIO_QUERRTY_ON,
+	AMP_GPIO_OFF = 0,
+	AMP_GPIO_ON,
+	AMP_GPIO_QUERRTY_ON,
 };
 
 enum AMP_S4_STATUS {
-	 AMP_S4_AUTO = 0,
-	 AMP_S4_PWM,
+	AMP_S4_AUTO = 0,
+	AMP_S4_PWM,
 };
 
-#define QUERY_IMMED		msecs_to_jiffies(0)
-#define QUERY_LATTER		msecs_to_jiffies(200)
-#define AMP_SENSE_READY		0x80
+struct rt55xx_platform_data {
+	uint32_t gpio_rt55xx_enable;
+	uint32_t gpio_rt55xx_power;
+	const char *power_supply;
+	struct rpm_regulator *power_reg;
+};
 
-#define AMP_IOCTL_MAGIC		'g'
-#define AMP_SET_CONFIG		_IOW(AMP_IOCTL_MAGIC, 0x01, unsigned)
-#define AMP_READ_CONFIG		_IOW(AMP_IOCTL_MAGIC, 0x02, unsigned)
-#define AMP_SET_MODE		_IOW(AMP_IOCTL_MAGIC, 0x03, unsigned)
-#define AMP_SET_PARAM		_IOW(AMP_IOCTL_MAGIC, 0x04, unsigned)
-#define AMP_WRITE_REG		_IOW(AMP_IOCTL_MAGIC, 0x07, unsigned)
-#define AMP_QUERY_OM		_IOW(AMP_IOCTL_MAGIC, 0x08, unsigned)
+struct rt55xx_reg_data {
+	unsigned char addr;
+	unsigned char val;
+};
+
+struct rt55xx_config {
+	unsigned int reg_len;
+	struct rt55xx_reg_data reg[MAX_REG_DATA];
+};
+
+struct rt55xx_comm_data {
+	unsigned int out_mode;
+	struct rt55xx_config config;
+};
+
+struct rt55xx_config_data {
+	unsigned int mode_num;
+	struct rt55xx_comm_data cmd_data[RT55XX_MAX_MODE];  /* [mode][mode_kind][reserve][cmds..] */
+};
+
+#define RT55XX_IOCTL_MAGIC	'g'
+//#define RT55XX_SET_CONFIG	_IOW(RT55XX_IOCTL_MAGIC, 0x01, unsigned)
+//#define RT55XX_READ_CONFIG	_IOR(RT55XX_IOCTL_MAGIC, 0x02, unsigned)
+#define RT55XX_SET_MODE		_IOW(RT55XX_IOCTL_MAGIC, 0x03, int)
+#define RT55XX_SET_PARAM	_IOW(RT55XX_IOCTL_MAGIC, 0x04, struct rt55xx_config_data)
+//#define RT55XX_WRITE_REG	_IOW(RT55XX_IOCTL_MAGIC, 0x07, unsigned)
+#define RT55XX_QUERY_OM		_IOR(RT55XX_IOCTL_MAGIC, 0x08, int)
 
 #endif
-
